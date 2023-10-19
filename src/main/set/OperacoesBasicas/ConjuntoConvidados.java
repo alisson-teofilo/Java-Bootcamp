26975816f6f4607f1da605fc7c66c41b32b025c5
Alisson-teofilo <alissonteofilo@gmail.com>
2023-10-18 21:53:47 -0300

    set

--- src/main/set/OperacoesBasicas/ConjuntoConvidados.java
+++ src/main/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -1,73 +1,60 @@
 package main.set.OperacoesBasicas;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ConjuntoConvidados {
-  //atributo
-  private Set<Convidado> convidadosSet;
+    private Set<Convidado> convidadoSet;
 
-  public ConjuntoConvidados() {
-    this.convidadosSet = new HashSet<>();
-  }
+    public ConjuntoConvidados(){
+        this.convidadoSet = new HashSet<>();
+    }
 
-  public void adicionarConvidado(String nome, int codigoConvite) {
-    convidadosSet.add(new Convidado(nome, codigoConvite));
-  }
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add( new Convidado(nome, codigoConvite));
+    }
 
-  public void removerConvidadoPorCodigoConvite(int codigoConvite) {
-    Convidado convidadoParaRemover = null;
-    if (!convidadosSet.isEmpty()) {
-      for (Convidado c : convidadosSet) {
-        if (c.getCodigoConvite() == codigoConvite) {
-          convidadoParaRemover = c;
-          break;
+    public void removerConvidado(int codigoConvidado){
+        Convidado convidadoRemover = null;
+        if(!convidadoSet.isEmpty()){
+            for(Convidado c: convidadoSet){
+                if(c.getCodigoConvite() == codigoConvidado){
+                    convidadoRemover = c;
+                    break;
+                }
+            }
+            convidadoSet.remove(convidadoRemover);
+        }else {
+            System.out.println("A lista de convidados está vazia");
         }
-      }
-      convidadosSet.remove(convidadoParaRemover);
-    } else {
-      throw new RuntimeException("O conjunto está vazio!");
+
+        if(convidadoRemover == null){
+            System.out.println("Não há convidados para remover");
+        }
     }
-  }
 
-  public int contarConvidados() {
-    return convidadosSet.size();
-  }
-
-  public void exibirConvidados() {
-    if (!convidadosSet.isEmpty()) {
-      System.out.println(convidadosSet);
-    } else {
-      System.out.println("O conjunto está vazio!");
+    public Set<Convidado> exibeListaConvidado(){
+        return convidadoSet;
     }
-  }
 
-  public static void main(String[] args) {
-    // Criando uma instância da classe ConjuntoConvidados
-    ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+    public int contarConvidado(){
+        return convidadoSet.size();
+    }
 
-    // Exibindo o número de convidados no conjunto (deve ser zero)
-    System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+    public static void main(String[] args) {
+        ConjuntoConvidados convidados = new ConjuntoConvidados();
 
-    // Adicionando convidados ao conjunto
-    conjuntoConvidados.adicionarConvidado("Alice", 1234);
-    conjuntoConvidados.adicionarConvidado("Bob", 1235);
-    conjuntoConvidados.adicionarConvidado("Charlie", 1235);
-    conjuntoConvidados.adicionarConvidado("David", 1236);
+        convidados.adicionarConvidado("Alisson", 1234);
+        convidados.adicionarConvidado("Vitória", 4321);
+        convidados.adicionarConvidado("Dinha", 1604);
+        convidados.adicionarConvidado("Dinha2", 1604);
+        convidados.adicionarConvidado("Wesley", 3101);
 
-    // Exibindo os convidados no conjunto
-    System.out.println("Convidados no conjunto:");
-    conjuntoConvidados.exibirConvidados();
+        convidados.removerConvidado(1604);
+        System.out.println(convidados.exibeListaConvidado());
+        System.out.println(convidados.contarConvidado());
+    }
 
-    // Exibindo o número atualizado de convidados no conjunto
-    System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
 
-    // Removendo um convidado do conjunto por código de convite
-    conjuntoConvidados.removerConvidadoPorCodigoConvite(1236);
-    System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados após a remoção");
-
-    // Exibindo os convidados atualizados no conjunto
-    System.out.println("Convidados no conjunto após a remoção:");
-    conjuntoConvidados.exibirConvidados();
-  }
 }
--- src/main/set/OperacoesBasicas/Convidado.java
+++ src/main/set/OperacoesBasicas/Convidado.java
@@ -3,40 +3,45 @@ package main.set.OperacoesBasicas;
 import java.util.Objects;
 
 public class Convidado {
-  //atributos
-  private String nome;
-  private int codigoConvite;
+    private String nome;
+    private int codigoConvite;
 
-  public Convidado(String nome, int codigoConvite) {
-    this.nome = nome;
-    this.codigoConvite = codigoConvite;
-  }
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", codigoConvite=" + codigoConvite +
+                '}';
+    }
 
-  public String getNome() {
-    return nome;
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Convidado convidado)) return false;
+        return getCodigoConvite() == convidado.getCodigoConvite();
+    }
 
-  public int getCodigoConvite() {
-    return codigoConvite;
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigoConvite());
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (!(o instanceof Convidado convidado)) return false;
-    return getCodigoConvite() == convidado.getCodigoConvite();
-  }
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+    public String getNome(){
+        return nome;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(getCodigoConvite());
-  }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public int getCodigoConvite(){
+        return codigoConvite;
+    }
 
-  @Override
-  public String toString() {
-    return "Convidado{" +
-        "nome='" + nome + '\'' +
-        ", codigoConvite=" + codigoConvite +
-        '}';
-  }
+    public void setCodigoConvite(int codigoConvite){
+        this.codigoConvite = codigoConvite;
+    }
 }
